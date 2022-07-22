1e0abd8995cd3ce99dfc8a28f7986cb2d336fa4d
Josh Schmitz <josh.schmitz1@gmail.com>
2022-07-22 15:54:35 -0500

    Delete htdocs/animalallies directory

--- htdocs/animalallies/AnimalAlliesDatabase.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE aa_lostandfound;
-
-CREATE TABLE pet_lostandfound (
-	entry_id 		INT(11)			NOT NULL	UNIQUE,
-	description 	TEXT			NOT NULL,
-	pet_image 		VARCHAR(150),
-	thumbnail 		VARCHAR(150),
-	date 			DATE,
-PRIMARY KEY(entry_id));
